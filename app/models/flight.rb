b03c9d75ebf6a2bb54f12716bf54fa6b46eb618b
ngperrin <ngperrin@protonmail.com>
2020-01-11 11:19:09 -0600

    Finished Screen 1

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,8 +1,5 @@
 class Flight < ApplicationRecord
 
-#  belongs_to :from, :class_name => "Airport"
-#  belongs_to :to,   :class_name => "Airport"
-
 
   validates :start_id,  presence: true
   validates :finish_id, presence: true
